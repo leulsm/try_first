5f49146aac973630bfe45a9e1d1cbb14201b3962
leulsm <leulsolm7@gmail.com>
2022-07-22 20:46:34 +0300

    initial

---
+++ app/src/main/java/com/leul/service/MainActivity.java
@@ -0,0 +1,36 @@
+package com.leul.service;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button stop,start;
+    CustomService cs;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        stop=findViewById(R.id.btn_stop);
+        start=findViewById(R.id.btn_start);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService(new Intent(MainActivity.this,CustomService.class));
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopService(new Intent(MainActivity.this,CustomService.class));
+            }
+        });
+    }
+}
